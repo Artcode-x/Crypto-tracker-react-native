e75d3609abd22140e97d9710fe18c0e960ccafc5
Alex <aironer@yandex.ru>
2024-08-12 12:34:08 +0300

    Стилизация нижнего меню 1. Импорт иконок из библиотеки react-native-vector-icons

--- components/AppRoute/AppRoute.js
+++ components/AppRoute/AppRoute.js
@@ -3,16 +3,52 @@ import { NavigationContainer } from "@react-navigation/native"
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import Main from "../../pages/Main"
 import Favorite from "../../pages/AnotherScreen/AnotherScreen"
+import { Ionicons } from "react-native-vector-icons"
+import { StyleSheet } from "react-native"
 
 const Tab = createBottomTabNavigator()
 
 export const AppRoute = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={Main} options={{ title: "CryptoTracker" }} />
-        <Tab.Screen name="Favorite" component={Favorite} options={{ title: "Избранное" }} />
+      <Tab.Navigator
+        style={styles.tabBarStyle}
+        screenOptions={{
+          tabBarActiveTintColor: "#FF6347", // Цвет активного элемента
+          tabBarInactiveTintColor: "#aaa", // Цвет неактивного элемента
+        }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={Main}
+          options={{
+            title: "CryptoTracker",
+            tabBarIcon: ({ color }) => <Ionicons name="home-outline" size={24} color={color} />,
+          }}
+        />
+        <Tab.Screen
+          name="Favorite"
+          component={Favorite}
+          options={{
+            title: "Избранное",
+            tabBarIcon: ({ color }) => <Ionicons name="star-outline" size={24} color={color} />,
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   )
 }
+
+const styles = StyleSheet.create({
+  tabBarStyle: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    elevation: 0,
+    backgroundColor: "#fff",
+    borderTopColor: "#e4e4e4",
+    borderTopWidth: 1,
+    height: 60,
+  },
+})
