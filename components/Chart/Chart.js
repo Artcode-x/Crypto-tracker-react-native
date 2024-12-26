1f5b80eb6c0935830ea07aa98e8d33335832d50a
Artcode <aironer@ya.ru>
2024-12-27 00:18:49 +0300

    Исправлено отображение дней на графике 1. Если выбран таймфрейм 30D, показываем каждую вторую метку даты, чтобы корректно уместить/отобразить дни.

--- components/Chart/Chart.js
+++ components/Chart/Chart.js
@@ -1,24 +1,43 @@
-import { ActivityIndicator, Dimensions, Modal, Text, TouchableOpacity, View } from "react-native";
-import { LineChart } from "react-native-chart-kit";
-import { styles } from './Chart.styles'
-import { useDispatch, useSelector } from "react-redux";
-import { setChartDays } from "../../store/reducersSlice";
-import { daysSelector } from "../../store/toolkitSelectors";
-
-export const Chart = ({selectedCoinData, chartData,  modalVisible, closeModal, isloading, coinHistoryData}) => {
-    
-// selectedCoinData - тут лежат данные для свечных графиков
-
-const chartDays = useSelector(daysSelector)
-const dispatch = useDispatch()
-
+import {
+    ActivityIndicator,
+    Dimensions,
+    Modal,
+    Text,
+    TouchableOpacity,
+    View,
+  } from "react-native";
+  import { LineChart } from "react-native-chart-kit";
+  import { styles } from "./Chart.styles";
+  import { useDispatch, useSelector } from "react-redux";
+  import { setChartDays } from "../../store/reducersSlice";
+  import { daysSelector } from "../../store/toolkitSelectors";
+  import { useState } from "react";
+  
+  export const Chart = ({
+    selectedCoinData,
+    chartData,
+    modalVisible,
+    closeModal,
+    isloading,
+    coinHistoryData,
+  }) => {
+    // selectedCoinData - тут лежат данные для свечных графиков
+    const [is30DSelected, setIs30DSelected] = useState(false);
+    const chartDays = useSelector(daysSelector);
+    const dispatch = useDispatch();
+  
     const openChart = (days) => {
-        dispatch(setChartDays(days))
+      if (days === 30) {
+        setIs30DSelected(true);
+      } else {
+        setIs30DSelected(false);
       }
-
-    return ( 
-        <Modal
-        animationType="slide"
+      dispatch(setChartDays(days));
+    };
+  
+    return (
+      <Modal
+        animationType='slide'
         transparent={true}
         visible={modalVisible}
         onRequestClose={closeModal}
@@ -28,111 +47,162 @@ const dispatch = useDispatch()
             {selectedCoinData && (
               <>
                 <Text style={styles.modalTitle}>{selectedCoinData.name}</Text>
-                {isloading ? (
-                  <ActivityIndicator size="large" color="#0000ff" />
-                ) : null}
-
+                {isloading ? <ActivityIndicator size='large' color='#0000ff' /> : null}
+  
                 {/* График */}
                 <View>
-  {coinHistoryData.length > 0 && (
-    <>
-      <View style={{
-        bottom: 0,
-        left: 0,
-        right: 0,
-        height: 'auto', 
-      backgroundColor: chartData.prices[chartData.prices.length - 1] > chartData.prices[0]
-          ? 'green' 
-          : 'red', 
-      }} />
-      
-      <LineChart
-        data={{
-          labels: chartData.labelDate,
-          datasets: [
-            {
-              data: chartData.prices,
-              strokeWidth: 4,
-              color: (opacity = 1) => {
-                const firstPrice = chartData.prices[0]
-                const lastPrice = chartData.prices[chartData.prices.length - 1];        
-                return lastPrice > firstPrice
-                  ? `rgba(0, 255, 0, ${opacity})` // зеленый цвет при росте
-                  : `rgba(255, 0, 0, ${opacity})`; // красный цвет при падении
-              },
-            },
-          ],
-        }}
-        width={Dimensions.get("window").width * 0.9} // Ширина графика
-        height={400}
-        chartConfig={{
-          backgroundColor: "#ffffff",
-          backgroundGradientFrom: "#ffffff",
-          backgroundGradientTo: "#ffffff",
-          decimalPlaces: 2,
-          color: (opacity = 1) => `rgba(0, 0, 0, ${opacity * 0})`, // Цвет линий
-          labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`, // Цвет меток
-          style: {
-            borderRadius: 16,
-            borderWidth: 1,
-          },
-          propsForDots: {
-            r: "0", // радиус 0, чтобы скрыть точки
-          },
-          propsForLabels: {
-            fontSize: 10, // Уменьшение шрифта меток
-          },
-        }}
-        style={{
-          marginVertical: 10,
-          borderRadius: 16,
-          elevation: 10,
-        }}
-      />
-    </>
-  )}
-</View>
-
-
-{isloading ? (null) : ( 
-<>
-
-<View>
-    <Text style={{fontWeight: 'bold', marginBottom: '15'}}>
-    Current Price: {selectedCoinData?.current_price} $
-    </Text>
-</View>
-
-    <Text>Выбранный диапазон дней: {chartDays}</Text>
-    <View style={styles.chartButtons}>
-      <TouchableOpacity onPress={() => openChart(1)}>
-        <Text style={[styles.chartButton, chartDays === 1 && styles.activeButton]}>
-          <Text style={[styles.buttonText, chartDays === 1 && styles.activeButtonText]}>24H</Text>
-        </Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => openChart(7)}>
-        <Text style={[styles.chartButton, chartDays === 7 && styles.activeButton]}>
-          <Text style={[styles.buttonText, chartDays === 7 && styles.activeButtonText]}>7D</Text>
-        </Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => openChart(14)}>
-        <Text style={[styles.chartButton, chartDays === 14 && styles.activeButton]}>
-          <Text style={[styles.buttonText, chartDays === 14 && styles.activeButtonText]}>14D</Text>
-        </Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => openChart(30)}>
-        <Text style={[styles.chartButton, chartDays === 30 && styles.activeButton]}>
-          <Text style={[styles.buttonText, chartDays === 30 && styles.activeButtonText]}>30D</Text>
-        </Text>
-      </TouchableOpacity>
-    </View>
-  </>
-
- )}
-                <TouchableOpacity
-                  onPress={closeModal}
-                  style={styles.closeButton}
-                >
+                  {coinHistoryData.length > 0 && (
+                    <>
+                      <View
+                        style={{
+                          bottom: 0,
+                          left: 0,
+                          right: 0,
+                          height: "auto",
+                          backgroundColor:
+                            chartData.prices[chartData.prices.length - 1] >
+                            chartData.prices[0]
+                              ? "green"
+                              : "red",
+                        }}
+                      />
+  
+                      <LineChart
+                        // data={{
+                        //   labels: chartData.labelDate,
+                        // data={{
+                        //   labels: chartData.labelDate.filter((_, index) => index % 2 === 0), // Показываем каждую вторую метку даты для всех таймфреймов
+                        data={{
+                          labels: is30DSelected
+                            ? chartData.labelDate.filter((_, index) => index % 2 === 0) // Показываем каждую вторую метку даты только если выбран 30D таймфрейм
+                            : chartData.labelDate, // Показываем все метки, если не 30D
+                          datasets: [
+                            {
+                              data: chartData.prices,
+                              strokeWidth: 4,
+                              color: (opacity = 1) => {
+                                const firstPrice = chartData.prices[0];
+                                const lastPrice =
+                                  chartData.prices[chartData.prices.length - 1];
+                                return lastPrice > firstPrice
+                                  ? `rgba(0, 255, 0, ${opacity})` // зеленый цвет при росте
+                                  : `rgba(255, 0, 0, ${opacity})`; // красный цвет при падении
+                              },
+                            },
+                          ],
+                        }}
+                        width={Dimensions.get("window").width * 0.9} // Ширина графика
+                        height={400}
+                        chartConfig={{
+                          backgroundColor: "#ffffff",
+                          backgroundGradientFrom: "#ffffff",
+                          backgroundGradientTo: "#ffffff",
+                          decimalPlaces: 2,
+                          color: (opacity = 1) => `rgba(0, 0, 0, ${opacity * 0})`, // Цвет линий
+                          labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`, // Цвет меток
+                          style: {
+                            borderRadius: 16,
+                            borderWidth: 1,
+                          },
+                          propsForDots: {
+                            r: "0", // радиус 0, чтобы скрыть точки
+                          },
+                          propsForLabels: {
+                            fontSize: 10, // Уменьшение шрифта меток
+                          },
+                        }}
+                        style={{
+                          marginVertical: 10,
+                          borderRadius: 16,
+                          elevation: 10,
+                        }}
+                      />
+                    </>
+                  )}
+                </View>
+  
+                {isloading ? null : (
+                  <>
+                    <View>
+                      <Text style={{ fontWeight: "bold", marginBottom: "15" }}>
+                        Current Price: {selectedCoinData?.current_price} $
+                      </Text>
+                    </View>
+  
+                    <Text>Выбранный диапазон дней: {chartDays}</Text>
+                    <View style={styles.chartButtons}>
+                      <TouchableOpacity onPress={() => openChart(1)}>
+                        <Text
+                          style={[
+                            styles.chartButton,
+                            chartDays === 1 && styles.activeButton,
+                          ]}
+                        >
+                          <Text
+                            style={[
+                              styles.buttonText,
+                              chartDays === 1 && styles.activeButtonText,
+                            ]}
+                          >
+                            24H
+                          </Text>
+                        </Text>
+                      </TouchableOpacity>
+                      <TouchableOpacity onPress={() => openChart(7)}>
+                        <Text
+                          style={[
+                            styles.chartButton,
+                            chartDays === 7 && styles.activeButton,
+                          ]}
+                        >
+                          <Text
+                            style={[
+                              styles.buttonText,
+                              chartDays === 7 && styles.activeButtonText,
+                            ]}
+                          >
+                            7D
+                          </Text>
+                        </Text>
+                      </TouchableOpacity>
+                      <TouchableOpacity onPress={() => openChart(14)}>
+                        <Text
+                          style={[
+                            styles.chartButton,
+                            chartDays === 14 && styles.activeButton,
+                          ]}
+                        >
+                          <Text
+                            style={[
+                              styles.buttonText,
+                              chartDays === 14 && styles.activeButtonText,
+                            ]}
+                          >
+                            14D
+                          </Text>
+                        </Text>
+                      </TouchableOpacity>
+                      <TouchableOpacity onPress={() => openChart(30)}>
+                        <Text
+                          style={[
+                            styles.chartButton,
+                            chartDays === 30 && styles.activeButton,
+                          ]}
+                        >
+                          <Text
+                            style={[
+                              styles.buttonText,
+                              chartDays === 30 && styles.activeButtonText,
+                            ]}
+                          >
+                            30D
+                          </Text>
+                        </Text>
+                      </TouchableOpacity>
+                    </View>
+                  </>
+                )}
+                <TouchableOpacity onPress={closeModal} style={styles.closeButton}>
                   <Text style={styles.closeButtonText}>Close</Text>
                 </TouchableOpacity>
               </>
@@ -140,5 +210,6 @@ const dispatch = useDispatch()
           </View>
         </View>
       </Modal>
-    )
-}
+    );
+  };
+  
