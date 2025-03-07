ed1232557798f031f073d4ef75707a1456348b4a
Art <aironer@yandex.ru>
2025-03-07 21:47:28 +0300

    Исправление лоадера в новостях. 1. Применение стилей к ActivityIndicator

--- components/News/News.js
+++ components/News/News.js
@@ -37,10 +37,6 @@ export default function News() {
     getNews()
   }, [filter, language])
 
-  if (loading) {
-    return <ActivityIndicator size='large' color='#0000ff' />
-  }
-
   const select = (button) => {
     switch (button) {
       case 1:
@@ -73,105 +69,135 @@ export default function News() {
 
   return (
     <View style={styles.container}>
-      <FlatList
-        style={{
-          backgroundColor: "rgba(50, 48, 49, 0.8)",
-          marginLeft: "4%",
-          marginRight: "4%",
-          marginBottom: "38%",
-          borderWidth: 1,
-          borderColor: "wheat",
-          borderRadius: 20,
-          marginTop: 4
-        }}
-        data={news}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => (
-          <View style={styles.item}>
-            <Text style={styles.title}>{item.title}</Text>
-            <Text style={styles.createdAt}>
-              {format(new Date(item.created_at), "dd MMMM yyyy", {
-                locale: datePost
-              })}
-            </Text>
+      {loading ? (
+        <ActivityIndicator size='large' color='red' style={{ paddingTop: "5%" }} />
+      ) : (
+        <>
+          <FlatList
+            style={{
+              backgroundColor: "rgba(50, 48, 49, 0.8)",
+              marginLeft: "4%",
+              marginRight: "4%",
+              marginBottom: "38%",
+              borderWidth: 1,
+              borderColor: "wheat",
+              borderRadius: 20,
+              marginTop: 4
+            }}
+            data={news}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({ item }) => (
+              <View style={styles.item}>
+                <Text style={styles.title}>{item.title}</Text>
+                <Text style={styles.createdAt}>
+                  {format(new Date(item.created_at), "dd MMMM yyyy", {
+                    locale: datePost
+                  })}
+                </Text>
+              </View>
+            )}
+          />
+          <View style={styles.box}>
+            <View style={styles.chartButtons}>
+              <TouchableOpacity onPress={() => select(1)}>
+                <Text
+                  style={[
+                    styles.chartButton,
+                    filter === "bullish" && styles.activeButton
+                  ]}
+                >
+                  <Text
+                    style={[
+                      styles.buttonText,
+                      filter === "bullish" && styles.activeButtonText
+                    ]}
+                  >
+                    Bullish
+                  </Text>
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => select(2)}>
+                <Text
+                  style={[
+                    styles.chartButton,
+                    filter === "bearish" && styles.activeButton
+                  ]}
+                >
+                  <Text
+                    style={[
+                      styles.buttonText,
+                      filter === "bearish" && styles.activeButtonText
+                    ]}
+                  >
+                    Bearish
+                  </Text>
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => select(3)}>
+                <Text
+                  style={[
+                    styles.chartButton,
+                    filter === "important" && styles.activeButton
+                  ]}
+                >
+                  <Text
+                    style={[
+                      styles.buttonText,
+                      filter === "important" && styles.activeButtonText
+                    ]}
+                  >
+                    Important
+                  </Text>
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => select(4)}>
+                <Text
+                  style={[styles.chartButton, filter === "hot" && styles.activeButton]}
+                >
+                  <Text
+                    style={[
+                      styles.buttonText,
+                      filter === "hot" && styles.activeButtonText
+                    ]}
+                  >
+                    Hot
+                  </Text>
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.chartButtons}>
+              <TouchableOpacity onPress={() => choseLang("ru")}>
+                <Text
+                  style={[styles.chartButton2, language === "ru" && styles.activeButton]}
+                >
+                  <Text
+                    style={[
+                      styles.buttonText,
+                      language === "ru" && styles.activeButtonText
+                    ]}
+                  >
+                    Ru
+                  </Text>
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => choseLang("en")}>
+                <Text
+                  style={[styles.chartButton2, language === "en" && styles.activeButton]}
+                >
+                  <Text
+                    style={[
+                      styles.buttonText,
+                      language === "en" && styles.activeButtonText
+                    ]}
+                  >
+                    En
+                  </Text>
+                </Text>
+              </TouchableOpacity>
+            </View>
           </View>
-        )}
-      />
-      <View style={styles.box}>
-        <View style={styles.chartButtons}>
-          <TouchableOpacity onPress={() => select(1)}>
-            <Text
-              style={[styles.chartButton, filter === "bullish" && styles.activeButton]}
-            >
-              <Text
-                style={[
-                  styles.buttonText,
-                  filter === "bullish" && styles.activeButtonText
-                ]}
-              >
-                Bullish
-              </Text>
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => select(2)}>
-            <Text
-              style={[styles.chartButton, filter === "bearish" && styles.activeButton]}
-            >
-              <Text
-                style={[
-                  styles.buttonText,
-                  filter === "bearish" && styles.activeButtonText
-                ]}
-              >
-                Bearish
-              </Text>
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => select(3)}>
-            <Text
-              style={[styles.chartButton, filter === "important" && styles.activeButton]}
-            >
-              <Text
-                style={[
-                  styles.buttonText,
-                  filter === "important" && styles.activeButtonText
-                ]}
-              >
-                Important
-              </Text>
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => select(4)}>
-            <Text style={[styles.chartButton, filter === "hot" && styles.activeButton]}>
-              <Text
-                style={[styles.buttonText, filter === "hot" && styles.activeButtonText]}
-              >
-                Hot
-              </Text>
-            </Text>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.chartButtons}>
-          <TouchableOpacity onPress={() => choseLang("ru")}>
-            <Text style={[styles.chartButton2, language === "ru" && styles.activeButton]}>
-              <Text
-                style={[styles.buttonText, language === "ru" && styles.activeButtonText]}
-              >
-                Ru
-              </Text>
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => choseLang("en")}>
-            <Text style={[styles.chartButton2, language === "en" && styles.activeButton]}>
-              <Text
-                style={[styles.buttonText, language === "en" && styles.activeButtonText]}
-              >
-                En
-              </Text>
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </View>
+        </>
+      )}
     </View>
   )
 }
